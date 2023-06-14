26e0b36245091d30dc6da6c0659dec2059c993fb
**** <****>
2023-06-14 21:49:36 +0300

    inde.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+// Multiplication of two numbers equal to a specific integer
+let multiply = 62;
+let num1 = 31;
+let num2 = 2;
+function multiply (num1, num2){
+    Number(num1) * Number(num2)
+}
+multiply;
 
-// Write your code here
+//To generate integers that are greater than 0
+let random = Math.floor(Math.random() * 10)
+
+//To calculate a remainder equal to 4
+let num3 = 12;
+let num4 = 8;
+let mod = num3 - num4;
+
+//To select the largest number in an array
+const max = Math.max(20,17,4,5,7,19);
